f73fa57746d89c45950694dc18c4bb6581747123
fbranda <francescorss6@gmail.com>
2021-03-10 12:31:58 +0100

    Update places.py

--- geograpy3/places.py
+++ geograpy3/places.py
@@ -168,8 +168,7 @@ class PlaceContext(object):
             self.set_regions()
 
         cur = self.conn.cursor()
-        table_query = "SELECT * FROM cities WHERE city_name IN (" + \
-                      ",".join("?" * len(self.places)) + ")"
+        table_query = "SELECT city_name FROM cities"
         cur.execute(table_query, self.places)
         rows = cur.fetchall()
 
